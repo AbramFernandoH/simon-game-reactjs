d597836332520a5afa44d32a451c7b2d78ab26ca
AbramFernandoH <abramfernando18@gmail.com>
2021-07-19 18:02:17 +0700

    Game is complete

--- src/Boxes.js
+++ src/Boxes.js
@@ -4,7 +4,7 @@ import "./Boxes.css";
 class Boxes extends Component{
   render(){
     return(
-      <div className={`Boxes ${this.props.bgColor}`} onClick={this.props.boxClick} style={{ backgroundColor: this.props.bgColor }}></div>
+      <button className={`Boxes ${this.props.bgColor}`} onClick={this.props.boxClick} disabled={this.props.boxDisabled} ></button>
     )
   }
 }
--- src/SimonGame.js
+++ src/SimonGame.js
@@ -13,7 +13,9 @@ class SimonGame extends Component{
       level: 0,
       pattern: [],
       userPattern: [],
-      isGameStart: false
+      isGameStart: false,
+      isClicked: 0,
+      btnDisabled: true
     }
     this.handleClick = this.handleClick.bind(this);
     this.startTheGame = this.startTheGame.bind(this);
@@ -21,19 +23,48 @@ class SimonGame extends Component{
 
   startTheGame(){
     this.setState(curState => ({isGameStart: true, level: curState.level + 1}));
+    this.gamePattern();
   }
 
-  handleClick(userClick){
-    this.setState(curState => ({ userPattern: [...curState.userPattern, userClick] }));
+  gamePattern(){
+    const pickColor = this.props.boxColors[Math.floor(Math.random() * this.props.boxColors.length)];
+    this.setState(curState => ({ pattern: [...curState.pattern, pickColor], userPattern: [], btnDisabled: true }));
+    document.querySelector(`.Boxes.${pickColor}`).style.borderColor = 'white';
+    document.querySelector(`.Boxes.${pickColor}`).style.backgroundColor = 'white';
+    setTimeout(() => {
+      document.querySelector(`.Boxes.${pickColor}`).style.borderColor = 'black';
+      document.querySelector(`.Boxes.${pickColor}`).style.backgroundColor = pickColor;
+      this.setState(curState => ({ btnDisabled: false }));
+    }, 300);
+  }
+
+  async handleClick(userClick){
+    if(this.state.isGameStart === true){
+      await this.setState(curState => ({ userPattern: [...curState.userPattern, userClick] }));
+      if(this.state.userPattern[this.state.isClicked] === this.state.pattern[this.state.isClicked]){
+        await this.setState(curState => ({ isClicked: curState.isClicked + 1 }));
+        if(this.state.userPattern.length === this.state.pattern.length){
+          await this.setState(curState => ({ isClicked: 0, level: curState.level + 1 }));
+          setTimeout(() => {
+            this.gamePattern();
+          }, 500);
+          return;
+        }
+        return;
+      } else {
+        await this.setState(curState => ({ isGameStart: false, pattern: [], level: 0 }));
+      }
+    } else {
+      alert('Please click start game button');
+    }
   }
 
   render(){
     return(
       <div className="SimonGame">
-        <h2 className="sub-title">{this.state.isGameStart === false ? 'Please type any key to start the game' : `Level ${this.state.level}`}</h2>
-        {this.state.isGameStart === false && <button className="start-game" onClick={this.startTheGame}>Start the game</button>}
+        {this.state.isGameStart === false ? <button className="start-game" onClick={this.startTheGame}>Start the game</button> : <h2 className="sub-title">Level {this.state.level}</h2> }
         <section className="the-boxes">
-          {this.props.boxColors.map(color => <Boxes bgColor={color} boxClick={() => this.handleClick(color)} />)}
+          {this.props.boxColors.map(color => <Boxes key={color} bgColor={color} boxClick={() => this.handleClick(color)} boxDisabled={this.state.btnDisabled} />)}
         </section>
       </div>
     )
